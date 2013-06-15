60489d97cf7f23fd199149cabd80ceacba082624
liwei <victor1233@163.com>
2013-06-15 11:50:28 +0800

    fix fancy plugin bug

--- plugins/photos_tag.rb
+++ plugins/photos_tag.rb
@@ -114,11 +114,14 @@ module Jekyll
 
     def render(context)
       # Convert the entire content array into one large string
-      lines = super.map(&:strip).join("\n")
+      content = super
+      #lines = super.map(&:strip).join("\n")
       # Get a unique identifier based on content
-      md5 = Digest::MD5.hexdigest(lines)
+      md5 = Digest::MD5.hexdigest(content)
+      #md5 = Digest::MD5.hexdigest(lines)
       # split the text by newlines
-      lines = lines.split("\n")
+      lines = content.split("\n")
+      #lines = lines.split("\n")
 
       p = PhotosUtil.new(context)
       list = ""
